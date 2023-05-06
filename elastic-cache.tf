d8e41d35e3f12158711f83b8128b1695f5f68960
unknown <anilkumarravuri510@gmail.com>
2023-05-06 12:44:23 +0530

    auto push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -1,9 +1,10 @@
+# CREATES CLUSTER
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "robot-${var.ENV}-redis"
   engine               = "redis"
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
-  parameter_group_name = aws_elasticache_parameter_group.default.name
+  parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
   engine_version       = "6.x"
   port                 = 6379
   security_group_ids   =  [aws_security_group.allow_redis.id]
@@ -11,8 +12,8 @@ resource "aws_elasticache_cluster" "redis" {
 }
 
 #CREATES PARAMETER GROUP
-resource "aws_elasticache_parameter_group" "default" {
-  name            = "robot-${var.ENV}-redis"
+resource "aws_elasticache_parameter_group" "redis_pg" {
+  name            = "robot-${var.ENV}-redis-pg"
   family          = "redis6.x"
 }
 
@@ -39,37 +40,3 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
 
 
 
-# # CREATING A CLUSTER OF DOCUMENT DB
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier = "robot-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-#   //backup_retention_period = 5
-#   //preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
-#   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet-group.name
-#   vpc_security_group_ids  =[aws_security_group.allow_mongodb.id]
-# }
-
-# # CREATES SUBNET GROUP NEEDED TO HOST THE DOCDB CLUSTER
-# resource "aws_docdb_subnet_group" "docdb_subnet-group" {
-#   name       = "robot-${var.ENV}-docdb-subnet-group"
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
-#   tags = {
-#     Name = "robot-${var.ENV}-docdb-subnet-group"
-#   }
-# }
-
-# output "output_ref" {
-#     value = data.terraform_remote_state.vpc
-# }
-
-# # CREATES INSTANCES NEEDED FOR THE DOCDB CLUSTER
-# resource "aws_docdb_cluster_instance" "cluster_instances" {
-#   count              = 1
-#   identifier         = "robot-${var.ENV}-docdb-instance"
-#   cluster_identifier = aws_docdb_cluster.docdb.id
-#   instance_class     = "db.t3.medium"
-# }
