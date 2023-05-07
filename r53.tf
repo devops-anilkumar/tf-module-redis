1e080133e612bfc72e8450e8cedccf47a4d016ac
unknown <anilkumarravuri510@gmail.com>
2023-05-07 17:43:18 +0530

    auto push

--- r53.tf
+++ r53.tf
@@ -1,8 +1,8 @@
 # THIS CREATES THE C NAME RECORD FOR REDIS
-resource "aws_route53_record" "redis" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "redis-${var.ENV}"
-  type    = "CNAME"
-  ttl     = 10
-  records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
-}
+# resource "aws_route53_record" "redis" {
+#   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+#   name    = "redis-${var.ENV}"
+#   type    = "CNAME"
+#   ttl     = 10
+#   records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
+# }
