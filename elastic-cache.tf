5559695e20cc91ebf5c0458ff9aa36a6eea9c069
unknown <anilkumarravuri510@gmail.com>
2023-05-08 12:50:53 +0530

    auto push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -2,11 +2,11 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "robot-${var.ENV}-redis"
   engine               = "redis"
-  node_type            = "cache.t3.small"
-  num_cache_nodes      = 1
+  node_type            = var.ELASTIC_CACHE_NODE_TYPE
+  num_cache_nodes      = var.ELASTIC_CACHE_NODE_COUNT 
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.x"
-  port                 = 6379
+  engine_version       = var.ELASTIC_CACHE_ENGINE_VERSION
+  port                 = var.ELASTIC_CACHE_PORT
   security_group_ids   =  [aws_security_group.allow_redis.id]
   subnet_group_name    =  aws_elasticache_subnet_group.redis_subnet_group.name
 }
@@ -14,7 +14,7 @@ resource "aws_elasticache_cluster" "redis" {
 #CREATES PARAMETER GROUP
 resource "aws_elasticache_parameter_group" "default" {
   name            = "robot-${var.ENV}-redis"
-  family          = "redis6.x"
+  family          = "redis${var.ELASTIC_CACHE_ENGINE_VERSION}"
 }
 
 # CREATES SUBNET GROUP
--- security-group.tf
+++ security-group.tf
@@ -6,16 +6,16 @@ resource "aws_security_group" "allow_redis" {
 
   ingress {
     description      = "allow docdb from local network"
-    from_port        = 6379
-    to_port          = 6379
+    from_port        = var.ELASTIC_CACHE_PORT
+    to_port          = var.ELASTIC_CACHE_PORT
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
 
   ingress {
     description      = "allow docdb from default network"
-    from_port        = 6379
-    to_port          = 6379
+    from_port        = var.ELASTIC_CACHE_PORT
+    to_port          = var.ELASTIC_CACHE_PORT
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
